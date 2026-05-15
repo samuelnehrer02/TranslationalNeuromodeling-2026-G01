38ce9f74bca615a8ce4d59f304b88ce31de2d1eb
samuelnehrer02 <nehrer.samuel@gmail.com>
2026-05-15 11:02:02 +0200

    step02: initiate work

--- Analysis/step01/step01.jl
+++ Analysis/step01/step01.jl
@@ -18,16 +18,18 @@ include("model_4.jl")
 
 # --- Model I: Fixed T, Bellman equation planning (free params: β₁, β₂, p, θ_T) ---
 cognitive_model_1 = create_cognitive_model_1()
-agent_model_1     = init_agent(cognitive_model_1, save_history = true)
+agent_model_1 = init_agent(cognitive_model_1, save_history = true)
 
 # --- Model II: Learned T, Bellman equation planning (free params: β₁, β₂, p) ---
 cognitive_model_2 = create_cognitive_model_2()
-agent_model_2     = init_agent(cognitive_model_2, save_history = true)
+agent_model_2 = init_agent(cognitive_model_2, save_history = true)
 
 # --- Model III: Fixed T, Bellman softmax planning (free params: β₁, β₂, p, θ_T) ---
 cognitive_model_3 = create_cognitive_model_3()
-agent_model_3     = init_agent(cognitive_model_3, save_history = true)
+agent_model_3 = init_agent(cognitive_model_3, save_history = true)
 
 # --- Model IV: Learned T, Bellman softmax planning (free params: β₁, β₂, p) ---
 cognitive_model_4 = create_cognitive_model_4()
-agent_model_4     = init_agent(cognitive_model_4, save_history = true)
+agent_model_4  = init_agent(cognitive_model_4, save_history = true)
+
+
